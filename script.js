2f21972872daf7387e5a86f9efe585fc818e4c25
adelina2999 <tataru.adelina99@gmail.com>
2020-02-17 19:50:58 +0200

    Lesson 8, guessing the random number

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-let array = [];
-for (let i = 2014; i < 2051; i++) {
-    let time = new Date(i, 0, 1);
-    if( time.getDay() == 0){
-        array.push(time.getFullYear())
-    }
+let min = 1;
+let max = 10;
+var random = Math.floor(Math.random() * (+max - +min)) + +min;
+console.log("Random number: " + random);
+
+const x = prompt("Introdu un numar intreg intre 1 si 10");
+
+if(x == random){
+    console.log("Good Work");
+} else {
+    console.log("Not matched");
 }
-console.log(array);
