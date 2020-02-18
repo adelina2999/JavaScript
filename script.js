4d32f48db7d0d7d54f93bdb620d3c1c8e2ae6e12
adelina2999 <tataru.adelina99@gmail.com>
2020-02-18 23:15:03 +0200

    Converting degrees

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
-function multiplyValues() {
-    let num1, num2, res;
-    num1=Number(document.calculator.txtnum1.value);
-    num2=Number(document.calculator.txtnum2.value);
-    res=num1*num2;
-    document.calculator.txtres.value=res;
+function convertCelsiusToFahrenheit(c) {
+    let f = c * 9 / 5 + 32;  
+    console.log(f)
 }
 
-function divideValues() {
-    let num1, num2, res;
-    num1=Number(document.calculator.txtnum1.value);
-    num2=Number(document.calculator.txtnum2.value);
-    res=num1/num2;
-    document.calculator.txtres.value=res;
-}
+convertCelsiusToFahrenheit(60)
+convertCelsiusToFahrenheit(1000)
+convertCelsiusToFahrenheit(1)
+convertCelsiusToFahrenheit(54)
