4b6da22c641e86d51339e3aadc0e384fecd4d13f
adelina2999 <tataru.adelina99@gmail.com>
2020-02-18 17:06:47 +0200

    Lesson 10, calculator that calculates the multiplication and divion of two numbers

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
-function countDaysUntilXmas() {
-    let minute = 60 * 1000
-    let hour = minute * 60
-    let day = hour * 24
-    let today = new Date()
-    let nextXmas = new Date()
-    nextXmas.setMonth(11)
-    nextXmas.setDate(25)
-    if (today.getMonth() == 11 && today.getDate() > 25) {
-        nextXmas.setFullYear(nextXmas.getFullYear() + 1)
-    }
-    var diff = nextXmas.getTime() - today.getTime()
-    diff = Math.floor(diff/day)
-    return diff
+function multiplyValues() {
+    let num1, num2, res;
+    num1=Number(document.calculator.txtnum1.value);
+    num2=Number(document.calculator.txtnum2.value);
+    res=num1*num2;
+    document.calculator.txtres.value=res;
 }
-let x = ( "You have " + countDaysUntilXmas()  + " days until Christmas." );
-console.log(x);
+
+function divideValues() {
+    let num1, num2, res;
+    num1=Number(document.calculator.txtnum1.value);
+    num2=Number(document.calculator.txtnum2.value);
+    res=num1/num2;
+    document.calculator.txtres.value=res;
+}
