3add3d33c1fe8fe0554045b2069110f106f96c7d
adelina2999 <tataru.adelina99@gmail.com>
2020-02-18 23:20:46 +0200

    Lesson 12, geting the URL

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-function convertCelsiusToFahrenheit(c) {
-    let f = c * 9 / 5 + 32;  
-    console.log(f)
-}
-
-convertCelsiusToFahrenheit(60)
-convertCelsiusToFahrenheit(1000)
-convertCelsiusToFahrenheit(1)
-convertCelsiusToFahrenheit(54)
+console.log(window.location)
