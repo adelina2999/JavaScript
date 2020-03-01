7721c30b0a3d8f7e25059ed2ec1ae325fed2d492
adelina2999 <tataru.adelina99@gmail.com>
2020-03-01 17:21:38 +0200

    Lesson 21, add py to a string

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
-function signOfNumbers(a, b){
-  if(a < 0){
-    console.log("The sign of the first number is minus")
-  } else {
-    console.log("The sign of the first number is plus")
-  }
-  
-  if(b < 0){
-    console.log("The sign of the second number is minus")
-  } else {
-    console.log("The sign of the second number is plus")
-  }
+let word = "pyggoogle";
+if (word[0] != "p" && word[1] != "y") {
+  word = "py" + word;
 }
-
-signOfNumbers(76, -87)
+console.log(word)
