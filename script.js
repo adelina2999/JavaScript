6609b748b0b762b1212f488ba288eb7495b5d87c
adelina2999 <tataru.adelina99@gmail.com>
2020-03-01 17:02:02 +0200

    Lesson 19, check a given integer

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-function test50(a, b) 
-{
-  return ((a == 50 || b == 50) || (a + b == 50));
+function firstFunction(a) {
+  if(a >= 20 && a <= 100){
+    console.log("Este in intervalul 20 - 100");
+  }      
+
 }
-console.log(test50(50, 50))
-console.log(test50(20, 50))
-console.log(test50(20, 20))
-console.log(test50(20, 30))
+
+function secondFunction(b) {
+  if(b >= 100 && b <= 400){
+    console.log("Este in intervalul 100 ");
+  }
+}
+firstFunction(150);
+secondFunction(45);
