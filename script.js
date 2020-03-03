52aa5883567a8c29ebf1d5bf88d66ae55ab69561
adelina2999 <tataru.adelina99@gmail.com>
2020-03-03 22:18:08 +0200

    Lesson 22, remove a character from a string

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-let word = "pyggoogle";
-if (word[0] != "p" && word[1] != "y") {
-  word = "py" + word;
-}
-console.log(word)
+function remove_character(str, char_pos) 
+ {
+  part1 = str.substring(0, char_pos);
+  part2 = str.substring(char_pos + 1, str.length);
+  return part1 + part2;
+ }
+
+console.log(remove_character("Adelina",5));
