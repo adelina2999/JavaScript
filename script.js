ca03aa4a1f15584a3087d5234dfdfdc123576a4f
adelina2999 <tataru.adelina99@gmail.com>
2020-04-01 15:15:12 +0300

    Lesson 28, check whether two given integers values are in the range 50-99

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-function trueOrFalse(string){
-    return string.startsWith("Java");
-}
-console.log(trueOrFalse("Javavirus"));
-console.log(trueOrFalse("Coronavirus"));
+function test50to99(a, b){
+    return (a >= 50 && a <= 99) && (b >= 50 && b <= 99);
+}  
+console.log(test50to99(1, 54));
+console.log(test50to99(54, 78));
+console.log(test50to99(1, 101));
