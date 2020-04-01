bb2f883d18a055879ecd297b842128299f109f76
adelina2999 <tataru.adelina99@gmail.com>
2020-04-01 14:20:28 +0300

    Lesson 27, check wheter a string starts with 'Java'

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-let string = "coronavirus"
-let array = string.split('');
-let last3DigitsOfString = array[array.length - 3] + array[array.length - 2] + array[array.length - 1]
-let newString = last3DigitsOfString + string + last3DigitsOfString;
-
-console.log(newString)
+function trueOrFalse(string){
+    return string.startsWith("Java");
+}
+console.log(trueOrFalse("Javavirus"));
+console.log(trueOrFalse("Coronavirus"));
