179f9957a035b53a8144e5434d28c6271bf877c0
adelina2999 <tataru.adelina99@gmail.com>
2020-04-02 16:47:54 +0300

    Lesson 40, check if a number is 8 or the result is 8

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-function sumOfTwoIntegers(int1, int2) {
-    if(int1 + int2 >= 50 && int1 + int2 <= 80) {
-        return 65;
-    } else {
-        return 80;
+function theValueOfEight(val1, val2) {
+    if((val1 == 8 || val2 == 8) || (val1 == 8 && val2 == 8)) {
+        return "The value is 8"
+    } else if (val1 + val2 == 8 || val1 - val2 == 8 || val2 - val1 == 8){
+        return "The result is 8"
     }
 }
-console.log(sumOfTwoIntegers(39,14));
-console.log(sumOfTwoIntegers(1, 2));
-console.log(sumOfTwoIntegers(1, 81));
+console.log(theValueOfEight(1, 8));
+console.log(theValueOfEight(8, 8));
+console.log(theValueOfEight(18, 10));
+console.log(theValueOfEight(10, 18));
+console.log(theValueOfEight(1, 7));
