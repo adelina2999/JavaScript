c7de340b409758d8235082502f654077798aa9f9
adelina2999 <tataru.adelina99@gmail.com>
2020-04-02 11:05:46 +0300

    Lesson 33, checking the range of 2 numbers

--- script.js
+++ script.js
@@ -1,21 +1,10 @@
-let a = 134;
-let b = 67;
-
-function getDistanceToOneHundred(num){
-    if(num < 100) {
-        return 100 - num;
-     } else if(num > 100) { 
-        return num - 100;
-     }
+const checkRange = (number, param1, param2) => number >= param1 && number <= param2
+const a = 71
+const b = 99
+if(checkRange(a, 40, 60) && checkRange(b, 40, 60)){
+    console.log("Is in range 40-60")
+}else if (checkRange(a, 70, 100) && checkRange(b, 70, 100)){
+    console.log("Is in range 70-100")
+}else {
+    console.log("They are not in the range")
 }
-
-function getNearestNumberTo100() {
-    if (getDistanceToOneHundred(a) < getDistanceToOneHundred(b)) {
-        console.log(`${a} is the nearest`)
-    } else if (getDistanceToOneHundred(a) > getDistanceToOneHundred(b)) {
-        console.log(`${b} is the nearest`)
-    }
-
-}
-
-getNearestNumberTo100();
