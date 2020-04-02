fe508263f9b2e522f979ddbdfe98d3b70e9fd9bd
adelina2999 <tataru.adelina99@gmail.com>
2020-04-02 13:18:03 +0300

    Lesson 35, check if a specifiedchar exists in a string

--- script.js
+++ script.js
@@ -1,25 +1,11 @@
-function numberFrom40to60(a) {
-    if(a >= 40 && a <= 60) {
+function specifiedCharacter(a) {
+    const string = "carantina"
+    if(a == string[2] || a == string[3] || a == string[4]) {
         return true
     }
+
     return false
 }
-
-function maxNumber(a, b) {
-  if(numberFrom40to60(a) && numberFrom40to60(b)) {
-      if(a > b) {
-          return "a is max"
-      }else if(b > a) {
-          return "b is max"
-      }else {
-          return "both are equal"
-      }
-  }
-  return "At least one number is not in range"
-}
-
-console.log(maxNumber(44, 45))
-console.log(maxNumber(12, 56))
-console.log(maxNumber(45, 44))
-console.log(maxNumber(12, 11))
-console.log(maxNumber(44, 44))
+console.log(specifiedCharacter("a"))
+console.log(specifiedCharacter("1"))
+console.log(specifiedCharacter("b"))
