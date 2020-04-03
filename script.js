ebd8cb0a8739550dccd270937c0c8a603c7ff463
adelina2999 <tataru.adelina99@gmail.com>
2020-04-03 16:06:45 +0300

    Reversing a given string

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-function mulptipleOfSeverOrEleven(x, y) {
-    if (!((x % 7 == 0 || x % 11 == 0) && (y % 7 == 0 || y % 11 == 0))) {
-        return (x % 7 == 0 || x % 11 == 0) || (y % 7 == 0 || y % 11 == 0)
-    } else {
-        return false
-    }
-} 
-console.log(mulptipleOfSeverOrEleven(14, 21));
-console.log(mulptipleOfSeverOrEleven(14, 20));
-console.log(mulptipleOfSeverOrEleven(77, 77));
+const ReverseString = str => [...str].reverse().join(''); 
+  
+console.log(ReverseString("Geeks for Geeks")) 
