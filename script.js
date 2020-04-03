fb7141335ce8df872c6503816b3b89249c025e35
adelina2999 <tataru.adelina99@gmail.com>
2020-04-03 18:10:10 +0300

    Lesson 49, replace the characters from a string with the next character from altphabet

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-const ReverseString = str => [...str].reverse().join(''); 
-  
-console.log(ReverseString("Geeks for Geeks")) 
+let alphabet = "abcdefghijklmnopqrstuvwxyz"
+function changeTheLetterOfString(str) {
+    let newStr = ''
+    for(let i = 0; i < str.length; i++) {
+        for(let l = 0; l < alphabet.length; l++) {
+            if (str[i] === alphabet[l]) {
+                newStr = newStr + alphabet[l + 1] 
+            }
+        }
+    }
+    
+    return newStr
+}
+
+console.log(changeTheLetterOfString("adelina"))
+console.log(changeTheLetterOfString("vadim"))
