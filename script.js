5ad6695569f38be6bc588ae837f2a26009914c03
adelina2999 <tataru.adelina99@gmail.com>
2020-04-03 15:49:22 +0300

    Lesson 46, the multiple of 7 & 11

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-function testNumber(x, y) {
-    return (x === 15 || y === 15 || x + y === 15 || Math.abs(x - y) === 15);
-}
-
-console.log(testNumber(15, 9));
-console.log(testNumber(7, 9));
+function mulptipleOfSeverOrEleven(x, y) {
+    if (!((x % 7 == 0 || x % 11 == 0) && (y % 7 == 0 || y % 11 == 0))) {
+        return (x % 7 == 0 || x % 11 == 0) || (y % 7 == 0 || y % 11 == 0)
+    } else {
+        return false
+    }
+} 
+console.log(mulptipleOfSeverOrEleven(14, 21));
+console.log(mulptipleOfSeverOrEleven(14, 20));
+console.log(mulptipleOfSeverOrEleven(77, 77));
