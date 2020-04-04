96f8e3f2bddc3d02e4870b98fe70fe7da45171db
adelina2999 <tataru.adelina99@gmail.com>
2020-04-04 17:52:31 +0300

    syntax

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
-let alphabet = "abcdefghijklmnopqrstuvwxyz"
-function changeTheLetterOfString(str) {
-    let newStr = ''
-    for(let i = 0; i < str.length; i++) {
-        for(let l = 0; l < alphabet.length; l++) {
-            if (str[i] === alphabet[l]) {
-                newStr = newStr + alphabet[l + 1] 
-            }
-        }
-    }
-    
-    return newStr
-}
+let name = 'Adelina'
+let age = 20
 
-console.log(changeTheLetterOfString("adelina"))
-console.log(changeTheLetterOfString("vadim"))
+console.log(name + ' ' + age )
+console.log(`${name} ${age}`)
