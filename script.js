221fa17c4bba006f3064196ebfb372aef3669307
adelina2999 <tataru.adelina99@gmail.com>
2020-04-04 18:14:05 +0300

    Arrays firts part

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
-let name = 'Adelina'
-let age = 20
+let array = [10, 11, 12, 13, 14]
 
-console.log(name + ' ' + age )
-console.log(`${name} ${age}`)
+let array1 = [10, 11, 12, 13, 14, 15, 16]
+array1.length = 3
+
+let array2 = [1, 2]
+array2.push('Adelina')
+array2.unshift('Vadim')
+
+let array3 = array2
+array3.pop()
+array3.shift()
+
+let matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+console.log(array1)
+console.log(array.length + " - array length") 
+console.log(array2 + " - added new elements to the array2")
+console.log(array3 + " - deleted the first and the last element from array")
+console.log(matrix[2][2])
+console.log(matrix[1][0])
+console.log(Array(5).fill(7) + " - initializing an array of 5 elements, and fill each element with 7")
