1969d903d2654669c07718be002e50be763c0de6
adelina2999 <tataru.adelina99@gmail.com>
2020-04-06 12:30:23 +0300

    Arrays part two

--- script.js
+++ script.js
@@ -1,26 +1,10 @@
-let array = [10, 11, 12, 13, 14]
+let array1 = [1, 2]
+let array2 = [3, 4]
+let array1_2 = array1.concat(array2)
+let array2_1 = [...array2, ...array1]
 
-let array1 = [10, 11, 12, 13, 14, 15, 16]
-array1.length = 3
 
-let array2 = [1, 2]
-array2.push('Adelina')
-array2.unshift('Vadim')
-
-let array3 = array2
-array3.pop()
-array3.shift()
-
-let matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-console.log(array1)
-console.log(array.length + " - array length") 
-console.log(array2 + " - added new elements to the array2")
-console.log(array3 + " - deleted the first and the last element from array")
-console.log(matrix[2][2])
-console.log(matrix[1][0])
-console.log(Array(5).fill(7) + " - initializing an array of 5 elements, and fill each element with 7")
+console.log(array1_2) 
+console.log(array2_1)
+console.log(array1_2.includes(2, 1)) //array1_2 contains 2 after position 1
+console.log(array1_2.includes(1, 2)) ////array1_2 does not contains 1 after position 2
