74459b1f15f07f7362c4e81b16110d5389a68221
adelina2999 <tataru.adelina99@gmail.com>
2020-04-06 13:02:53 +0300

    While loop

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-let array1 = [1, 2]
-let array2 = [3, 4]
-let array1_2 = array1.concat(array2)
-let array2_1 = [...array2, ...array1]
-
-
-console.log(array1_2) 
-console.log(array2_1)
-console.log(array1_2.includes(2, 1)) //array1_2 contains 2 after position 1
-console.log(array1_2.includes(1, 2)) ////array1_2 does not contains 1 after position 2
+let list = ['a', 'b', 'c']
+let i = 0
+while (i < list.length) {
+    console.log(list[i])  // value
+    console.log(i)  //index
+    i = i + 1
+}
