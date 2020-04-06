99ef43fb2ad0644ab60714c67b8e72f452c33369
adelina2999 <tataru.adelina99@gmail.com>
2020-04-06 13:12:56 +0300

    for...of loop

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let list = ['a', 'b', 'c']
-let i = 0
-do {
-    console.log(list[i]) //value
-    console.log(i) //index
-    i = i + 1
-} while (i < list.length)
+
+for (let i of list) {
+    console.log(i)
+
+}
