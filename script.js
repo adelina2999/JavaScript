acf25b82d65664f233067974826bfa464bf57636
adelina2999 <tataru.adelina99@gmail.com>
2020-04-06 17:12:32 +0300

    Objects

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-let list = ['a', 'b', 'c']
-
-for (let i of list) {
-    console.log(i)
-
+const car = {  
+    brand: 'Ford', 
+    model: 'Fiesta',  
+    start: function() {
+        let a = 5
+        return 'Started'  
+    },
+    array: [1, 2, 3],
+    stop: function() {
+        return 'Stoped'
+    },
+    availableColors: {
+        red: 'in stock',
+        blue: 'not in stock'
+    }
 }
+
+car.start()
+
+console.log(car.brand)
+console.log(car.start())
+console.log(car.stop())
+console.log(car.availableColors.red)
