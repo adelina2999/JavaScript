c3ae0eb3ece7da19b4966798ceee72b5c7b070cc
adelina2999 <tataru.adelina99@gmail.com>
2020-04-06 13:08:19 +0300

    do...while loop

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let list = ['a', 'b', 'c']
 let i = 0
-while (i < list.length) {
-    console.log(list[i])  // value
-    console.log(i)  //index
+do {
+    console.log(list[i]) //value
+    console.log(i) //index
     i = i + 1
-}
+} while (i < list.length)
