4f6aa3462bf0aaa951e4b7184f25c7e371ff49f4
adelina2999 <tataru.adelina99@gmail.com>
2020-04-07 16:07:53 +0300

    Functions 2, check if the string is palindrome

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-function numberReverse(x) {
-    return (
-        parseFloat(
-            x 
-            .toString()
-            .split('')
-            .reverse()
-            .join('')
-        ) * Math.sign(x)
-        
-    )
+function palindrome(word) {
+    var x = /[^A-Za-z0-0]/g
+    word = word.toLowerCase().replace(x, '')
+    let y = word.length
+    for (let i = 0; i < y/2; i++) {
+        if(word[i] !== word[y - 1 - i]) {
+            return false
+        }
+    }
+
+    return true
 }
 
-console.log(numberReverse(32242))
+console.log(palindrome('Adelina'))
+console.log(palindrome('Ana'))
