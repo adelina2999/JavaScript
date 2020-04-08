d173d261f1e8f95328b3e507a07e0cf0c1c21c6b
adelina2999 <tataru.adelina99@gmail.com>
2020-04-08 18:20:44 +0300

    Functions 5, convert the first letter of words to upper case

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-function alphabeticalOrder(str) {
-    let string = str.split('')
-    let sorted = string.sort()
-   return sorted.join('')
+function wordUpperCase (word) {
+    const array = word.split(" ")
+    const wordsOfArray = []
+
+    for(let i = 0; i < array.length; i++) {
+        wordsOfArray.push(array[i].split(""))
+    }
+
+    for(let i = 0; i < wordsOfArray.length; i++) {
+        wordsOfArray[i][0] = wordsOfArray[i][0].toUpperCase()    
+        wordsOfArray[i] = wordsOfArray[i].join("")
+    }
+
+
+    return wordsOfArray.join(" ")
 }
 
-console.log(alphabeticalOrder('adelina'))
+console.log(wordUpperCase("ana are mere rosii"))
