ecfce6880d5537804c79cc07457c2e74ff7449d1
adelina2999 <tataru.adelina99@gmail.com>
2020-04-12 10:26:16 +0300

    Array find(find a prime number in an array)

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-const inventory = [
-  {name: 'apples', quantity: 2},
-  {name: 'bananas', quantity: 0},
-  {name: 'cherries', quantity: 5}
-]
+function isPrime(element, index, array) {
+  let start = 2
+  while (start <= Math.sqrt(element)) {
+    if (element % start++ < 1) {
+      return false
+    }
+  }
 
-const result = inventory.find( ({ name })  => name === 'cherries')
-
-console.log(result)
+  return element > 1
+}
+console.log([4, 6, 8, 12].find(isPrime))
+console.log([4, 5, 8, 12].find(isPrime))
