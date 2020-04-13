4a3b71b69ccc18c5b15e85ddc4a62ec230bf9054
adelina2999 <tataru.adelina99@gmail.com>
2020-04-13 14:51:44 +0300

    Getting Started with JavaScript for Web Development Courses - Lesson 4 of 23

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-const isBelowThreshold = (currentValue) => currentValue < 40
-const array1 = [1, 30, 39, 29, 10, 13]
+const header = document.querySelector('h1')
 
-console.log(array1.every(isBelowThreshold))
-
-//The every() method tests whether all element in the array pass the test implemented 
-//by the provided function. It returns a Boolean value
+function changeText() {
+  header.innerText = 'Hello World!'
+}
