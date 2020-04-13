8285b2a381277de6c707cbb015fbf6dd051ebe96
adelina2999 <tataru.adelina99@gmail.com>
2020-04-13 10:00:37 +0300

    Array every()

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
-function isPrime(element, index, array) {
-  let start = 2
-  while (start <= Math.sqrt(element)) {
-    if (element % start++ < 1) {
-      return false
-    }
-  }
+const isBelowThreshold = (currentValue) => currentValue < 40
+const array1 = [1, 30, 39, 29, 10, 13]
 
-  return element > 1
-}
-console.log([4, 6, 8, 12].find(isPrime))
-console.log([4, 5, 8, 12].find(isPrime))
+console.log(array1.every(isBelowThreshold))
+
+//The every() method tests whether all element in the array pass the test implemented 
+//by the provided function. It returns a Boolean value
