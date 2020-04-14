8a603907ff1f4c2685b6c2d6fc4320382411683b
adelina2999 <tataru.adelina99@gmail.com>
2020-04-14 10:09:44 +0300

    GSWJSFWDC - Lesson 5 , Opperators

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
-const header = document.querySelector('h1')
+let a   = 10
+let b   = 3
+let c   = 15
+const d = '5'
+let e   = 'my name'
+const f = 'is Adelina'
 
-function changeText() {
-  header.innerText = 'Hello World!'
-}
+console.groupCollapsed('Addition')
+  console.log(a + b)
+  console.log(a + d)
+  console.log(e + ' ' + f)
+  console.log(e += ' is batman')
+console.groupEnd();
+
+console.groupCollapsed('Multiplication')
+  console.log(a * 3)
+  console.log(a * b)
+  console.log(b * c)
+console.groupEnd();
+
+a++
+b--
+--b
+c += a
+
+console.group('Incrementing')
+  console.log(a)   
+  console.log(b)
+  console.log(c)
+console.groupEnd()
