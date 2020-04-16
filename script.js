b0a14f014b230d8ca2d1e6d3d5e87a52df5b3e7d
adelina2999 <tataru.adelina99@gmail.com>
2020-04-16 16:18:47 +0300

    GSWJSFWDC - Lesson 1, DOM and DOM events

--- script.js
+++ script.js
@@ -1,49 +1,9 @@
-const first  = 'this is a string'
-const second = String('this is a string') 
+function goCooCoo() {
+  document.body.style.backgroundColor = '#C00C00'
+}
 
-console.groupCollapsed('Primitives vs Objects')
-  console.log(first === second)
-  console.log(
-    typeof first,
-    typeof String('this is a string'),
-    typeof new String()
-  )
-console.groupEnd()
-
-const sentence = 'this is my sentence'
-
-console.groupCollapsed('Strings')
-  console.log(first.toUpperCase()) 
-  console.log(second.toLowerCase())
-  console.log(sentence.startsWith('this is'))
-  console.log('👩🏻‍💻'.repeat(20))
-  console.log('      look at me goo'.trim())
-console.groupEnd()
-
-console.groupCollapsed('Numbers and Math') 
-console.log(1..toString())
-console.log((1).toString())
-console.log(Number.isInteger(5))
-console.log(Number.isInteger(5.5))
-console.log((1.435643).toFixed(4))
-console.log(
-  Math.random().toFixed(3),
-  Math.ceil(1.4),
-  Math.floor(6.5)
-)
-console.groupEnd()
-
-const myArray = ['adelina', 'vadim', 'severina']
-
-console.group('Arrays')
-  console.log(myArray.length)
-  console.log(myArray.join('-'))
-  console.log(myArray.push('adam'), myArray)
-  console.log(myArray.pop(), myArray)
-
-  myArray.forEach(function(user) {
-    console.log(user)
-  })
-
-  myArray.forEach((user) => console.log(user))
-console.groupEnd()
+const coffeeButton = document.querySelector('.coffee-btn')
+coffeeButton.onclick = function() {
+  document.body.style.backgroundColor = '#C0FFEE'
+}
+coffeeButton.onmouseenter = goCooCoo
