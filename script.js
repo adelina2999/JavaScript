2cea02f8557f8caa1fe6b030172de720181a9ba8
adelina2999 <tataru.adelina99@gmail.com>
2020-04-17 11:52:27 +0300

    GSWJSFWDC - Lesson 12, Pricing Calculator

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-function goCooCoo() {
-  document.body.style.backgroundColor = '#C00C00'
+const priceInput = document.querySelector('[name=price]')
+const quantityInput = document.querySelector('[name=quantity]')
+const total = document.querySelector('.total')
+const quantityLabel = document.querySelector('.quantity-label')
+
+function calculatePieCost() {
+  const price = priceInput.value
+  const quantity = quantityInput.value
+  const cost = price * quantity
+  console.log(cost)
+  total.innerText = '$' + cost.toFixed(2)
 }
 
-const coffeeButton = document.querySelector('.coffee-btn')
-coffeeButton.onclick = function() {
-  document.body.style.backgroundColor = '#C0FFEE'
+function updateQuantityLabel() {
+  const quantity = quantityInput.value
+  quantityLabel.innerText = quantity
 }
-coffeeButton.onmouseenter = goCooCoo
+
+calculatePieCost()
+
+priceInput.addEventListener('input', calculatePieCost)
+quantityInput.addEventListener('input', calculatePieCost)
+quantityInput.addEventListener('input', updateQuantityLabel)
