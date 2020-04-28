67d415af78495c45a8fc37b17938b24998a780fd
adelina2999 <tataru.adelina99@gmail.com>
2020-04-28 16:30:30 +0300

    Practice from You Don't Know JavaScript

--- script.js
+++ script.js
@@ -1,23 +1,52 @@
-const priceInput = document.querySelector('[name=price]')
-const quantityInput = document.querySelector('[name=quantity]')
-const total = document.querySelector('.total')
-const quantityLabel = document.querySelector('.quantity-label')
+let bankAccountBalance = prompt('What is your bank account balance?')
+//prices
+const TAX_RATE = prompt('Your state tax')
+const PHONE_PRICE = prompt('Phone price')
+const ACCESSORY_PRICE = prompt('Accessory price')
+const SPENDING_THRESHOLD = prompt('How much money do you want to spend?')
+//number of products
+let numberOfBoughtPhones = 0
+let numberOfBoughtAccessories = 0
 
-function calculatePieCost() {
-  const price = priceInput.value
-  const quantity = quantityInput.value
-  const cost = price * quantity
-  console.log(cost)
-  total.innerText = '$' + cost.toFixed(2)
+let freeBalance = bankAccountBalance - (bankAccountBalance - SPENDING_THRESHOLD)
+
+function purchase () {
+    const NUMBER_OF_PHONES_TO_BUY = Math.floor(freeBalance / valueWithTax(PHONE_PRICE))
+    for(let i = 0; i <= NUMBER_OF_PHONES_TO_BUY; i++) {
+        buyAPhone()
+    }
+
+    const NUMBER_OF_ACCESORIES_TO_BUY = Math.floor(freeBalance / valueWithTax(ACCESSORY_PRICE))
+    for(let i = 0; i <= NUMBER_OF_ACCESORIES_TO_BUY; i++) {
+        buyAnAccessory()
+    }
+    console.log(numberOfBoughtPhones)
+    console.log(numberOfBoughtAccessories)
+    console.log(freeBalance)
+    console.log(bankAccountBalance)
 }
 
-function updateQuantityLabel() {
-  const quantity = quantityInput.value
-  quantityLabel.innerText = quantity
+function valueWithTax (productPrice) {
+    productPrice = Number(productPrice)
+    return (productPrice + (productPrice * TAX_RATE/100)).toFixed(2)
 }
 
-calculatePieCost()
+function buyAPhone() {
+    const PHONE_PRICE_WITH_TAX = valueWithTax(PHONE_PRICE)
+    if(freeBalance > PHONE_PRICE_WITH_TAX) {
+        bankAccountBalance -= PHONE_PRICE_WITH_TAX 
+        freeBalance -= PHONE_PRICE_WITH_TAX
+        numberOfBoughtPhones++
+    }
+}
 
-priceInput.addEventListener('input', calculatePieCost)
-quantityInput.addEventListener('input', calculatePieCost)
-quantityInput.addEventListener('input', updateQuantityLabel)
+function buyAnAccessory() {
+    const ACCESSORY_PRICE_WITH_TAX = valueWithTax(ACCESSORY_PRICE)
+    if(freeBalance > ACCESSORY_PRICE_WITH_TAX) {
+        bankAccountBalance -= ACCESSORY_PRICE_WITH_TAX
+        freeBalance -= ACCESSORY_PRICE_WITH_TAX
+        numberOfBoughtAccessories++
+    }
+}
+
+purchase()
