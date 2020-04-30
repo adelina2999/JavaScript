3c4ea53b1d8ec77d0353f10944f57342a9ebffc5
adelina2999 <tataru.adelina99@gmail.com>
2020-04-30 15:46:59 +0300

    illustration of this. Indentifier from You don't know JS

--- script.js
+++ script.js
@@ -1,52 +1,19 @@
-let bankAccountBalance = prompt('What is your bank account balance?')
-//prices
-const TAX_RATE = prompt('Your state tax')
-const PHONE_PRICE = prompt('Phone price')
-const ACCESSORY_PRICE = prompt('Accessory price')
-const SPENDING_THRESHOLD = prompt('How much money do you want to spend?')
-//number of products
-let numberOfBoughtPhones = 0
-let numberOfBoughtAccessories = 0
-
-let freeBalance = bankAccountBalance - (bankAccountBalance - SPENDING_THRESHOLD)
-
-function purchase () {
-    const NUMBER_OF_PHONES_TO_BUY = Math.floor(freeBalance / valueWithTax(PHONE_PRICE))
-    for(let i = 0; i <= NUMBER_OF_PHONES_TO_BUY; i++) {
-        buyAPhone()
-    }
-
-    const NUMBER_OF_ACCESORIES_TO_BUY = Math.floor(freeBalance / valueWithTax(ACCESSORY_PRICE))
-    for(let i = 0; i <= NUMBER_OF_ACCESORIES_TO_BUY; i++) {
-        buyAnAccessory()
-    }
-    console.log(numberOfBoughtPhones)
-    console.log(numberOfBoughtAccessories)
-    console.log(freeBalance)
-    console.log(bankAccountBalance)
+function foo() {
+    console.log( this.bar )
 }
 
-function valueWithTax (productPrice) {
-    productPrice = Number(productPrice)
-    return (productPrice + (productPrice * TAX_RATE/100)).toFixed(2)
+var bar = 'global'
+
+var obj1 = {
+    bar: 'obj1',
+    foo: foo
 }
 
-function buyAPhone() {
-    const PHONE_PRICE_WITH_TAX = valueWithTax(PHONE_PRICE)
-    if(freeBalance > PHONE_PRICE_WITH_TAX) {
-        bankAccountBalance -= PHONE_PRICE_WITH_TAX 
-        freeBalance -= PHONE_PRICE_WITH_TAX
-        numberOfBoughtPhones++
-    }
+var obj2 = {
+    bar: 'obj2'
 }
 
-function buyAnAccessory() {
-    const ACCESSORY_PRICE_WITH_TAX = valueWithTax(ACCESSORY_PRICE)
-    if(freeBalance > ACCESSORY_PRICE_WITH_TAX) {
-        bankAccountBalance -= ACCESSORY_PRICE_WITH_TAX
-        freeBalance -= ACCESSORY_PRICE_WITH_TAX
-        numberOfBoughtAccessories++
-    }
-}
-
-purchase()
+foo()              //'global'
+obj1.foo()         //'obj1'
+foo.call( obj2 )   //'obj2'
+new foo()          //undefined
