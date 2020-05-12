9f9bc09e53e77cf08f9d432aaa868eed70f5276e
adelina2999 <tataru.adelina99@gmail.com>
2020-05-12 13:54:44 +0300

    You don't know JS, Types&Grammar, JSON-safe value suitable for stringification vs JSON string

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-function myFunction() {
-    var str = "Hello World"
-    var enc = window.btoa(str)
-    var dec = window.atob(enc)
-    var res = "Encoded String: " + enc + "<br>" + "Decoded String: " + dec
+var a = {
+    val: [1, 2, 3],
 
-document.getElementById("demo").innerHTML = res
+    //probably correct!
+    toJSON: function() {
+        return this.val.slice(1)
+    }
 }
+
+var b = {
+    val: [1, 2, 3], 
+
+    //probably incorrect!
+    toJSON: function() {
+        return '[' +
+        this.val.slice(1).join() +
+        ']'
+    }
+}
+console.log(JSON.stringify(a))
+console.log(JSON.stringify(b))
+
+/*in the second call, we stringified the returned string rather than the array itself
+wich was probably not what we wanted to*/
