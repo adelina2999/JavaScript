d02926d0755aa36e2dad0304769bd82bbf8e1096
adelina2999 <tataru.adelina99@gmail.com>
2020-05-15 10:39:35 +0300

    objects

--- script.js
+++ script.js
@@ -1,15 +1,73 @@
-function maxCharacterNumber() {
-  let arrayOfCharacters = ['Ayesha', 'Margaret', 'Anita', 'Savannah', 'Kira', 'Lilly', 'Gloria', 'Eleanor', 'Jemima']
-  let namesWithMaxNumberOfCharacters = []
-  let maxNumberOfCharacters = arrayOfCharacters[0]
-
-  for(let i = 1; i < arrayOfCharacters.length; i++) {
-    if(arrayOfCharacters[i].length >= maxNumberOfCharacters.length) {
-      maxNumberOfCharacters = arrayOfCharacters[i]
-      namesWithMaxNumberOfCharacters.push(arrayOfCharacters[i])
-
-    }
-  }
-  return namesWithMaxNumberOfCharacters
+let createUUID = () => {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8)
+    return v.toString(16)
+  })
 }
-console.log(maxCharacterNumber())
+
+let getTime = () => {
+  return new Date().getTime()
+}
+
+let visitors = [
+  {
+    id: createUUID(),
+    name: "Aishawolf Aiwolf",
+    date: getTime(),
+    phone: 9072007799
+  },
+  {
+    id: createUUID(),
+    name: "Hawys Olympia",
+    date: getTime(),
+    phone: 9072094948
+  },
+  {
+    id: createUUID(),
+    name: "Hollis Yaga",
+    date: getTime(),
+    phone: 9072218397
+  },
+  {
+    id: createUUID(),
+    name: "Hecate Asahina",
+    date: getTime(),
+    phone: 9072290817
+  }
+]
+
+visitors.push({
+  id: createUUID(),
+  name: "Adelina",
+  date: getTime(),
+  phone: 9072007799,
+  address: "Chisinau"
+})
+
+let addExclamationPointToNames = () => {
+  for(let j = 0; j < visitors.length; j++) {
+    visitors[j].name += '!'
+  }
+}
+
+addExclamationPointToNames()
+
+let addPropertyToObject = () => {
+  for(let i = 0; i < visitors.length; i++) 
+    visitors[i].city = 'Bucuresti'
+}
+addPropertyToObject()
+
+let showMyVisitors = () => {
+  for(let y = 0; y < visitors.length; y++)
+    console.log(visitors[y])
+}
+
+let concationationOfTwoStrings = () => 'Hello World'
+
+  for(let i = 0; i < visitors.length; i++)
+    visitors[i].myFunction = concationationOfTwoStrings
+
+showMyVisitors()
+
+console.log(visitors[0].myFunction())
