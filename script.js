d612f0f323534905dae6e3e583c911f971a68ab4
adelina2999 <tataru.adelina99@gmail.com>
2020-05-21 13:35:23 +0300

    Delete a number of elements from array

--- script.js
+++ script.js
@@ -1,73 +1,19 @@
-let createUUID = () => {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8)
-    return v.toString(16)
-  })
+let arrayOfNumbers = [1, 2, 3, 4, -1, -2, -3, -4]
+let chosenArray = []
+let numberOfElementsToBeDeleted = 100
+
+let lastChosenPosition = arrayOfNumbers.length - numberOfElementsToBeDeleted
+
+if(numberOfElementsToBeDeleted > arrayOfNumbers.length) {
+  console.log('Your number is bigger than my array')
+} else if(numberOfElementsToBeDeleted < 0) {
+  console.log('I need a positive number')
+} else if(typeof numberOfElementsToBeDeleted != 'number') {
+  console.log('I need a number')
 }
 
-let getTime = () => {
-  return new Date().getTime()
+for(let i = 0; i < lastChosenPosition; i++) {
+  chosenArray.push(arrayOfNumbers[i])
 }
 
-let visitors = [
-  {
-    id: createUUID(),
-    name: "Aishawolf Aiwolf",
-    date: getTime(),
-    phone: 9072007799
-  },
-  {
-    id: createUUID(),
-    name: "Hawys Olympia",
-    date: getTime(),
-    phone: 9072094948
-  },
-  {
-    id: createUUID(),
-    name: "Hollis Yaga",
-    date: getTime(),
-    phone: 9072218397
-  },
-  {
-    id: createUUID(),
-    name: "Hecate Asahina",
-    date: getTime(),
-    phone: 9072290817
-  }
-]
-
-visitors.push({
-  id: createUUID(),
-  name: "Adelina",
-  date: getTime(),
-  phone: 9072007799,
-  address: "Chisinau"
-})
-
-let addExclamationPointToNames = () => {
-  for(let j = 0; j < visitors.length; j++) {
-    visitors[j].name += '!'
-  }
-}
-
-addExclamationPointToNames()
-
-let addPropertyToObject = () => {
-  for(let i = 0; i < visitors.length; i++) 
-    visitors[i].city = 'Bucuresti'
-}
-addPropertyToObject()
-
-let showMyVisitors = () => {
-  for(let y = 0; y < visitors.length; y++)
-    console.log(visitors[y])
-}
-
-let concationationOfTwoStrings = () => 'Hello World'
-
-  for(let i = 0; i < visitors.length; i++)
-    visitors[i].myFunction = concationationOfTwoStrings
-
-showMyVisitors()
-
-console.log(visitors[0].myFunction())
+console.log(chosenArray)
